07d2f00652908d06ef890406ff333d7f3e9fc81e
Pavel Nekrasov <pavel@resultdevelopment.com>
2018-07-31 10:32:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const assert = require('assert'),
+	Rabbitmq = require('rabbitmq-boilerplate')
+
+module.exports = function () {
+	assert(!this.rabbitmq, "field exists")
+
+	this.rabbitmq = new Rabbitmq();
+
+	rabbitmq.config(this.config.get("rabbitmq.connection"));
+
+	return Promise.resolve();
+}
